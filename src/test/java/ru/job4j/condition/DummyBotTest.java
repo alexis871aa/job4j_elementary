9eb8cdafb5d79caae5c564e02b5784822e2a3595
Aleksey <alexis871aa@yandex.ru>
2023-04-17 08:41:39 +0300

    4.1.2. Глупый бот [#31323]

--- src/test/java/ru/job4j/condition/DummyBotTest.java
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -7,25 +7,25 @@ class DummyBotTest {
 
     @Test
     public void whenGreetBot() {
-        String in = new String("Hi, Bot.");
+        String in = "Hi, Bot.";
         String result = DummyBot.answer(in);
-        String expected = new String("Hi, SmartAss.");
+        String expected = "Hi, SmartAss.";
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenByeBot() {
-        String in = new String("Bye.");
+        String in = "Bye.";
         String result = DummyBot.answer(in);
-        String expected = new String("See you later.");
+        String expected = "See you later.";
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenUnknownBot() {
-        String in = new String("Can you add two plus two?");
+        String in = "Can you add two plus two?";
         String result = DummyBot.answer(in);
-        String expected = new String("I don't know. Please, ask another question.");
+        String expected = "I don't know. Please, ask another question.";
         assertThat(result).isEqualTo(expected);
     }
 }
