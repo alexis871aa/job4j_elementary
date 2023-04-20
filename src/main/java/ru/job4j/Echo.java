e1ea42404229e330eb3c73f749e19761cb4fb4c4
Aleksey <alexis871aa@yandex.ru>
2023-04-20 18:01:14 +0300

    53. Преобразовать трехзначное число в зависимости от условия

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -1,4 +1,22 @@
 package ru.job4j;
 
 public class Echo {
+    public static void transform(int number) {
+        int num1 = number / 100;
+        int num2 = number / 10 % 10;
+        int num3 = number % 10;
+
+        if (number % 2 == 0) {
+            System.out.println(num1 * 100 + num2 * 10 + num3);
+        }
+    }
+
+    public static void main(String[] args) {
+        transform(390);     // 281
+        transform(333);     // 444
+        transform(-900);    // -811
+        transform(999);     // 999
+        transform(-109);    // -209
+        transform(100);     // 11
+    }
 }
