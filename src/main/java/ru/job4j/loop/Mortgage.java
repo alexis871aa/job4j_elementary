ccb60449a317c44ae2fa68cc389db1e5d4c06784
Aleksey <alexis871aa@yandex.ru>
2023-04-27 14:14:40 +0300

    5.5. Простое число (оператор прерывания break) [#171694] исправление ошибки тестирования CI-бота

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,15 +3,16 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
+        percent /= 100;
         while (amount > 0) {
-            amount += amount * percent / 100 - salary;
+            amount += amount * percent - salary;
             year++;
         }
         return year;
     }
 
     public static void main(String[] args) {
-        int amount = 1000;
+        int amount = 1500;
         int salary = 1200;
         double percent = 50;
         System.out.println(year(amount, salary, percent));
